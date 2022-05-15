1a6d23fa111e7d4e4ed166aa4a159fe1c083ce3f
Ejiks <ivanbirulia@gmail.com>
2022-05-15 13:53:40 +0300

    finish code

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 from stack import Stack
 
+def balanser(unbalansed_str):
+    balans = Stack()
+    left_balans = "({["
+    right_balans = ")}]"
+    for item in unbalansed_str:
+        if item in left_balans:
+            balans.push(item)
+        elif item in right_balans:
+            if right_balans.find(item) == left_balans.find(balans.peek()):
+                balans.pop()
+            else:
+                balans.push(item)
+    if balans.size() != 0:
+        return "Несбалансированно"
+    return "Cбалансированно"
